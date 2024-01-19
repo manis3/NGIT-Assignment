543f363372b06d9eb321464efc55535b43a80953
Manish Hyongoju <m.hyongoju04@gmail.com>
2024-01-20 02:00:42 +0545

    Added json-server to host the JSON data and implemented an API call in the React project

---
+++ apps/assignment/src/apis/api.js
@@ -0,0 +1,12 @@
+// import { calculateAge } from "src/utils/extra";
+export const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:3040/datas");
+      const jsonData = await res.json();
+      return jsonData;
+  
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  
