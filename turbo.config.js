69cc30f906b76c1e9a322c081b94e1d3e1dab410
Manish Hyongoju <m.hyongoju04@gmail.com>
2024-01-19 23:22:34 +0545

    initiallization of project

---
+++ turbo.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projects: [
+    { 
+      name: "assignment-ui",
+      path: "assignment-ui" },
+    {
+      name: "assignement",
+      path: "apps/assignment",
+    },
+  ],
+};
